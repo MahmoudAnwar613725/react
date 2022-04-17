4bd3b25268bacea8ed101fff6cc352fd10a22a28
MahmoudAnwar613725 <mahmohamed@miu.edu>
2022-04-17 15:38:22 -0500

    lab-09 16-04-2022

---
+++ react-demo-1/src/containers/headers/Header.js
@@ -0,0 +1,23 @@
+import React from "react"
+import {Link} from 'react-router-dom';
+import './Header.css'
+
+ const Header = () => {
+
+    return (
+        <header>
+            <nav>
+                <ul>
+                    <li><Link to="/Posts"> Posts</Link></li>
+                    <li><Link to="/NewPost"> New Posts</Link></li>
+                </ul>
+
+            </nav>
+        </header>
+
+    )
+
+
+}
+
+export default Header
